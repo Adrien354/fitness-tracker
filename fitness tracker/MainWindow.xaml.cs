b30c807f68db794b3ed99e25ddcb48d71239a3b1
Adrian Filipek <adrqwer@outlook.com>
2023-12-09 20:31:13 +0100

    added

---
+++ fitness tracker/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace fitness_tracker
+{
+    public partial class MainWindow : Window
+    {
+        static float timer = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GetDate();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        public void GetDate()
+        {
+            date_box.Text = DateTime.Now.ToString();
+        }
+    }
+}
